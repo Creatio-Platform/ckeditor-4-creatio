13b3e71b83f3c1f712636380884cbc8100fe2b62
i.shupenya <i.shupenya@creatio.com>
2023-02-23 15:16:30 +0200

    Init repository

---
+++ plugins/creatioImageFileUpload/plugin.js
@@ -0,0 +1,39 @@
+CKEDITOR.plugins.add('creatioImageFileUpload',
+	{
+		lang: 'en,ru',
+		init: function(editor) {
+			editor.addCommand('creatioImageFileUploadCommand', {
+				allowedContent: {
+					img: {
+						attributes: '!src,srcset,alt,width,sizes'
+					}
+				},
+				exec: function() {
+					const hiddenUploadElement = CKEDITOR.dom.element.createFromHtml(
+						'<input type="file" accept="image/*" multiple="multiple">',
+					);
+					hiddenUploadElement.once('change', function (evt) {
+						const targetElement = evt.data.getTarget();
+						for (let index = 0; index < targetElement.$.files.length; index++) {
+							let dt = new DataTransfer();
+							dt.items.add( targetElement.$.files[index]);
+							editor.fire('paste', {
+								method: 'paste',
+								dataValue: '',
+								dataTransfer: new CKEDITOR.plugins.clipboard.dataTransfer({
+									files: dt.files,
+									types: ['Files'],
+								}),
+							});
+						}
+					});
+					editor.fire('CreatioImageClick', {}, editor);
+					hiddenUploadElement.$.click();
+				}
+			});
+			editor.ui.addButton('CreatioImageFileUploadButton', {
+				label: editor.lang.creatioImageFileUpload.label,
+				command: 'creatioImageFileUploadCommand'
+			});
+		}
+	});
